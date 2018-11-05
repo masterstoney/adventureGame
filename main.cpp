b760a067fbb8ce90c6b78887368e1999360288bf
masterstoney <tgmsstoney@gmail.com>
2018-11-04 23:31:04 -0600

    Fixed inverted commas

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 	string secondInput = "";
 
 	cout<<"Once upon a time, there lived two villages separated by a vast forest. One village laid to the east of the forest and the other to the west. The two villages loved to trade and commune with each other. What makes this story different than any other story is that this forest is a mystical forest that holds many enchanting adventures where any sort of adventure can happen whether big or small. The residents of each village travel through the forest almost every day to commune with (what the residents of each village call) their \"sister\" village. Recently, the east village lost contact with the west village, and many travelers have tried to go to the west village in the first place but gave up and went to the east village. They said something was always keeping them from getting there. The people in the east village have gone into the forest but never returned… Some have ventured around the forest but never returned… The story starts when a young person who was a traveler had just entered the village to the east of the forest."<<endl;
-	cout<<"\"Hello!\" said a voice to your right.\nYou turn to the right and see a guard dressed in all iron armor.\n\"My name is Phillip. What might be your business?\" asked Phillip.\nTYPE ‘nobleman’, ‘merchant’, or ‘commoner’"<<endl;
+	cout<<"\"Hello!\" said a voice to your right.\nYou turn to the right and see a guard dressed in all iron armor.\n\"My name is Phillip. What might be your business?\" asked Phillip.\nTYPE \'nobleman\', \'merchant\', or \'commoner\'"<<endl;
 	cin>>firstInput;
 	inputHandler(firstInput);
 	user.setType(matchInput<PlayerType>(firstInput,3,PlayerTypeNames));
